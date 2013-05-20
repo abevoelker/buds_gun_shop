2295afae45bc9bd5a1521c8a1900e6c057357eb9
Abe Voelker <abe@abevoelker.com>
2013-05-19 21:58:50 -0500

    Add Category object

---
+++ lib/buds_gun_shop/category.rb
@@ -0,0 +1,40 @@
+require 'active_model'
+require 'mechanize'
+require 'andand'
+
+module BudsGunShop
+  class Category
+    include ActiveModel::Validations
+
+    attr_accessor :name, :id, :parent, :children
+
+    validates :name, presence: true
+    validates :id,   presence: true
+
+    def initialize(attrs={})
+      attrs.each{|a,v| send(a.to_s+'=', v) }
+      self.children ||= []
+      self
+    end
+
+    def self.init_from_xml(xml, parent=nil)
+      xml.search('//category').map do |c|
+        id, name, is_leaf = ['id', 'name', 'isleaf'].map{|n| c.at(n).andand.text }
+        category = self.new(:name => name, :id => id, :parent => parent)
+        category.children = find(id) unless is_leaf
+        category
+      end
+    end
+
+    def self.find(id, session=Session.new)
+      page = session.get("http://www.budsgunshop.com/catalog/ajax/categories.php?cID=#{id}")
+      init_from_xml(page)
+    end
+
+    def self.all(session=Session.new)
+      root = session.get('http://www.budsgunshop.com/catalog/ajax/categories.php')
+      init_from_xml(root)
+    end
+
+  end
+end
---
+++ spec/unit/category_spec.rb
@@ -0,0 +1,71 @@
+require File.dirname(__FILE__) + '/unit_helper'
+
+class CategorySpec < MiniTest::Spec
+
+  describe 'Category' do
+
+    describe 'built without any values' do
+
+      before do
+        @category = BudsGunShop::Category.new
+      end
+
+      it "should not be valid" do
+        @category.wont_be :valid?
+      end
+
+    end
+
+    describe 'built with just name' do
+
+      before do
+        @category = BudsGunShop::Category.new(:name => 'Foo')
+      end
+
+      it "should not be valid" do
+        @category.wont_be :valid?
+      end
+
+    end
+
+    describe 'built with just id' do
+
+      before do
+        @category = BudsGunShop::Category.new(:id => '123')
+      end
+
+      it "should not be valid" do
+        @category.wont_be :valid?
+      end
+
+    end
+
+    describe 'built with name and id' do
+
+      before do
+        @category = BudsGunShop::Category.new(:name => 'Foo', :id => '123')
+      end
+
+      it "should be valid" do
+        @category.must_be :valid?
+      end
+
+    end
+
+    describe 'fetching all categories' do
+
+      before do
+        VCR.use_cassette('categories_all') do
+          @categories = BudsGunShop::Category.all
+        end
+      end
+
+      it "should create valid categories" do
+        @categories.each{|c| c.must_be :valid?}
+      end
+
+    end
+
+  end
+
+end
