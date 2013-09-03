2e1047dfab987ade7ef904b7d83e75d3cf995ced
Abe Voelker <abe@abevoelker.com>
2013-09-02 21:45:28 -0500

    Remove use of Agent in Manufacturer spec and clean up slightly

--- spec/unit/manufacturer_spec.rb
+++ spec/unit/manufacturer_spec.rb
@@ -2,14 +2,7 @@ require File.dirname(__FILE__) + '/unit_helper'
 
 describe BudsGunShop::Manufacturer do
 
-  before do
-    rate_limit = Celluloid::RateLimiter.new(100, 1)
-    Celluloid::Actor.clear_registry
-    Celluloid::Actor[:buds_gun_shop_agent] = BudsGunShop::Agent.new(rate_limit)
-  end
-
   describe 'built without any values' do
-
     before do
       @manufacturer = BudsGunShop::Manufacturer.new
     end
@@ -17,11 +10,9 @@ describe BudsGunShop::Manufacturer do
     it "should not be valid" do
       expect(@manufacturer).not_to be_valid
     end
-
   end
 
   describe 'built with just name' do
-
     before do
       @manufacturer = BudsGunShop::Manufacturer.new(name: 'Foo')
     end
@@ -29,11 +20,9 @@ describe BudsGunShop::Manufacturer do
     it "should not be valid" do
       expect(@manufacturer).not_to be_valid
     end
-
   end
 
   describe 'built with just id' do
-
     before do
       @manufacturer = BudsGunShop::Manufacturer.new(id: '123')
     end
@@ -41,11 +30,9 @@ describe BudsGunShop::Manufacturer do
     it "should not be valid" do
       expect(@manufacturer).not_to be_valid
     end
-
   end
 
   describe 'built with name and id' do
-
     before do
       @manufacturer = BudsGunShop::Manufacturer.new(name: 'Foo', id: '123')
     end
@@ -53,7 +40,6 @@ describe BudsGunShop::Manufacturer do
     it "should be valid" do
       expect(@manufacturer).to be_valid
     end
-
   end
 
 end
